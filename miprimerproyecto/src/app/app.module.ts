8a0e61aee3e436a861757851688ab2d5fbe00997
ProgramSeba <juarezdantesebastian2@gmail.com>
2023-02-27 16:01:31 -0300

    haciendo el login con boostrap

--- miprimerproyecto/src/app/app.module.ts
+++ miprimerproyecto/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { HttpClient, HttpClientModule} from "@angular/common/http";
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    TecnologiasComponent,
+    HabilidadesComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
